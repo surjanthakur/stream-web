f148585feb43b5493fc9ed3f20456b77ae659cc4
surjan thakur <tsurjan506@gmail.com>
2025-06-10 22:55:54 +0530

    save

--- routes/main.js
+++ routes/main.js
@@ -7,12 +7,12 @@ router.get("/", (req, res) => {
   res.render("homepage.ejs");
 });
 
-router.get("/meeting-code", wrapAsync, async (req, res, next) => {
+router.get("/meetingId", wrapAsync, async (req, res, next) => {
   const meetingHistory = await JoinMeeting.find({});
   res.render("get-meeting.ejs", { meetingHistory });
 });
 
-router.post("/meeting-code", wrapAsync, async (req, res, next) => {
+router.post("/meetingId", wrapAsync, async (req, res, next) => {
   const { meetingCode } = req.body;
   let newMeetingCode = new JoinMeeting({
     meetingCode: meetingCode,
