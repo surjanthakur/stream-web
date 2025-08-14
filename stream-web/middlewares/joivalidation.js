5f69631e644b0adaab4c199920ad5cafb65f99d0
surjanthakur <tsurjan506@gmail.com>
2025-08-15 03:30:23 +0530

    init save stream-web

--- stream-web/middlewares/joivalidation.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("joi");
-
-const Schema = Joi.object({
-  username: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(4).max(30).required(),
-});
-
-module.exports = Schema;
